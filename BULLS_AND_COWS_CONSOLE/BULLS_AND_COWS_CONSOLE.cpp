04a4028985c76286b84b0c7085bb4b7265d07c26
LazyHat <105165429+LazyHat@users.noreply.github.com>
2022-10-03 07:56:38 +0300

    developming

--- BULLS_AND_COWS_CONSOLE/BULLS_AND_COWS_CONSOLE.cpp
+++ BULLS_AND_COWS_CONSOLE/BULLS_AND_COWS_CONSOLE.cpp
@@ -10,11 +10,11 @@ using namespace std;
 uint16_t InputCountOfDigits() {
     string s;
     uint16_t countofdigits;
-    printf("Enter the number of digits, ranging from 3 to 10:\n");
+    printf("Enter the number of digits, ranging from 2 to 10:\n");
     cin >> s;
     try { countofdigits = stoi(s, nullptr, 16); }
     catch (const invalid_argument& ia) { cout << "Invalid argument: " << ia.what() << "\n"; exit(EXIT_FAILURE); }
-    if (countofdigits < 3) { printf("No-no-no, it's so easy, please enter value bigger than 2\n"); exit(EXIT_FAILURE); }
+    if (countofdigits < 2) { printf("No-no-no, it's so easy, please enter value bigger than 1\n"); exit(EXIT_FAILURE); }
     if (countofdigits > 10) { printf("Your value is too big for this game\n"); exit(EXIT_FAILURE); }
     return countofdigits;
 }
@@ -30,33 +30,13 @@ int main()
         switch (game) {
         case 1:
         {
-            Game session = Game(InputCountOfDigits());
-            do {
-                printf("Enter your value:\n");
-                int32_t usernum;
-                cin >> usernum;
-                session.SetUserValue(usernum);
-                session.CalculateBullsCows();
-                cout << session.GetGameResult();
-            } while (!session.GetGameResultBool());
+            Game session = Game(1, InputCountOfDigits());
+            cout << session.GetCompNumStr() << endl;
         }
             break;
         case 2:
         {
-            Game session = Game(InputCountOfDigits());
-            session.SetRandomCompNum();
-            cout << "My number: " << session.GetCompNum() << endl;
-            uint16_t x;
-            while (session.GetBulls() != session.GetCountOfDigits()) {
-                printf("Enter bulls: \n");
-                cin >> x;
-                session.SetBulls(x);
-                printf("Enter cows: \n");
-                cin >> x;
-                session.SetCows(x);
-                cout << "My number: " << session.GetNextNumber() << endl;
-            }
-            printf("I WIN!!! your value: %d\n", session.GetCompNum());
+
         }
             break;
         }
--- BULLS_AND_COWS_CONSOLE/Game.h
+++ BULLS_AND_COWS_CONSOLE/Game.h
@@ -2,138 +2,19 @@
 #define _CRT_RAND_S
 #include <stdint.h>
 #include <stdlib.h>
-#include <time.h>
-#include <math.h>
+#include <vector>
+#include <string>
+#include <random>
+
+using namespace std;
 class Game
 {
 private:
-	uint32_t compnum{}, usernum{};
-	uint16_t countofdigits{};
-	uint16_t* usernumarr{};
-	uint16_t* lastcompnumarr{};
-	uint16_t* compnumarr{};
-	uint16_t bulls{}, cows{};
-
+	vector<uint16_t> compnumvec;
 public:
-	Game() {
-		bulls = 0;
-		cows = 0;
-		countofdigits = 0;
-	}
-	//comp sets number, user solve
-	Game(uint16_t _countofdigits) {
-		countofdigits = _countofdigits;
-		bulls = 0;
-		cows = 0;
-		usernumarr = new uint16_t(countofdigits);
-		compnumarr = new uint16_t(countofdigits);
-		lastcompnumarr = new uint16_t(countofdigits);
-		srand(time(NULL));
-		do {
-			SetRandomCompNum();
-			ConvertIntToArray(compnum, compnumarr);
-		} while (!TestArrayDigits(compnumarr));
-	}
-
-	uint32_t GetCompNum() { return compnum; }
-	uint16_t GetCountOfDigits() { return countofdigits;  }
-	uint16_t GetBulls() { return bulls; }
-	uint16_t GetCows() { return cows; }
-	void SetBulls(uint16_t _bulls) { bulls = _bulls; }
-	void SetCows(uint16_t _cows) { cows = _cows; }
-
-	void SetRandomCompNum() {
-		do {
-			rand_s(&compnum);
-			compnum %= (int32_t)pow(10, countofdigits);
-		} while (!(compnum >= pow(10, countofdigits - 1)));
-	}
-
-	void SetUserValue(int32_t uservalue) {
-		usernum = uservalue;
-		ConvertIntToArray(usernum, usernumarr);
-	}
-
-	std::string GetGameResult() {
-		if (bulls == countofdigits) return "You win, the hidden number: " + std::to_string(compnum) + "\n";
-		else return "BULLS: " + std::to_string(bulls) + ("\nCOWS:") + std::to_string(cows) + "\n";
-	}
-
-	bool GetGameResultBool() {
-		return bulls == countofdigits;
-	}
-
-	uint32_t GetNextNumber() {
-		if ((bulls == 0 && cows == 0) || (bulls = 0 && cows == 1)) {
-			SetRandomCompNum();
-			ToEqualArraysCompNumLastNum();
-			return compnum;
-		}
-	}
-
-	void CalculateBullsCows() {
-		bulls = CompareArrays(compnumarr, usernumarr);
-		cows = CompareArraysNonEqualPlaces(compnumarr, usernumarr) - bulls;
-	}
-
+	string GetCompNumStr();
+	Game(uint16_t gamenum, uint16_t countOfDigits);
 private:
-
-private:
-
-	void MoveDigitsExeptOneCompNum(uint16_t excludedigitindex) {
-		uint16_t k;
-		usernumarr[0]++;
-		usernumarr[1] = excludedigitindex;
-		for (int16_t i = 0; i < countofdigits - 1; i++) {
-			if (i == excludedigitindex - 1) continue;
-			if (i == 0) {
-				k = compnumarr[countofdigits];
-
-			}
-		}
-	}
-
-	void ToEqualArraysCompNumLastNum(){
-		for (int16_t i = 0; i < countofdigits - 1; i++) {
-			lastcompnumarr[i] = compnumarr[i];
-		}
-		}
-
-	uint16_t CompareArrays(uint16_t* arr1, uint16_t* arr2) {
-		uint16_t countofequal = 0;
-		for (uint16_t i = 0; i <= countofdigits - 1; i++) {
-			if (arr1[i] == arr2[i]) countofequal++;
-		}
-		return countofequal;
-	}
-
-	uint16_t CompareArraysNonEqualPlaces(uint16_t* arr1, uint16_t* arr2) {
-		uint16_t k = 0;
-		for (int16_t i = 0; i < countofdigits; i++) {
-			for (int16_t j = 0; j < countofdigits; j++) {
-				if (arr1[i] == arr2[j]) k++;
-			}
-		}
-		return k;
-	}
-
-	void ConvertIntToArray(uint32_t numtoarr, uint16_t* arr) {
-		for (int16_t i = countofdigits - 1; i >= 0; i--) {
-			arr[i] = numtoarr % 10;
-			numtoarr /= 10;
-		}
-	}
-
-	bool TestArrayDigits(uint16_t* arr) {
-		int16_t k = 0;
-		for (int16_t i = 0; i < 10; i++) {
-			for (int16_t j = 0; j <= countofdigits - 1; j++) {
-				if (arr[j] == i) k++;
-			}
-			if (k < 2) k = 0;
-			else return false;
-		}
-		return true;
-	}
+	void FillVectorRandNum(vector<uint16_t> v);
+	int16_t Rand1digit();
 };
-
